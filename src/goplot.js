fed192608e64cada6e57da8013465b4ff0c00529
Michael Gargano <Michael.Gargano@jax.org>
2018-01-31 14:32:14 -0500

    WIP: Saving to branch

--- src/goplot.js
+++ src/goplot.js
@@ -16,9 +16,9 @@ export class GoPlot{
         height = 600 - margin.top - margin.bottom;
         var x0 = d3.scaleBand().range([0,width]).padding([.3])
         var x1 = d3.scaleBand();
-        var y = d3.scaleLinear().range([height, 0]);
-        var xAxis = d3.axisBottom(x0).scale(x0);
-        var yAxis = d3.axisLeft(y).scale(y);
+        var y = d3.scaleLinear().range([0,height]);
+        var xAxis = d3.axisLeft(x0).scale(x0);
+        var yAxis = d3.axisTop(y).scale(y);
         var canvas = d3.select(".goplot").append("svg")
                 .attr("width", width + margin.left + margin.right)
                 .attr("height",height + margin.top + margin.bottom)
@@ -34,34 +34,34 @@ export class GoPlot{
             x0.domain(categories);
             x1.domain(goTerms).range([0,x0.bandwidth()]);
             y.domain([0, d3.max(data, function(d) { return d3.max(d.terms, function(d) { return d.value; }); })]);
-            canvas.append("g").attr("class", "x-axis").attr("transform","translate(0," + height + ")")
-            .call(xAxis);
+            canvas.append("g").attr("class", "x-axis").call(xAxis);
             canvas.append("g").attr("class", "y axis").style('opacity','0').call(yAxis);
             canvas.select('.y').transition().duration(500).delay(1300).style('opacity', '1');
             // A slice is a Category on the bar graph
             var slice = canvas.selectAll(".slice")
             .data(data)
             .enter().append("g")
-            .attr("class","g").attr("transform",function(d) { return "translate(" + x0(d.category) + ",0)";});
+            .attr("class","g").attr("transform",function(d) { return "translate(0," + x0(d.category) + ")";});
             // Create a rectangle for each term and set the width and its color;
             slice.selectAll("rect")
             .data(function(d){return d.terms;})
-            .enter().append("rect").attr("width",x1.bandwidth())
-            .attr("x", function(d){ return x1(d.id)})
+            .enter().append("rect")
+            .attr("y", function(d){ return x1(d.id)})
+            .attr("x", function(d){ return y(0); })
             .style("fill", function(d,i){ 
                 var color = colors(category);
                 if(i == 2){
                     category++;
                 }
                 return color;
-            }).attr("y", function(d){return y(0); }).attr("height", function(d){ return height - y(0);});
+            }).attr("height", function(d){ return x1.bandwidth();});
             // Transition the height of each sub slice bar to the height of its value after a delay for effect.
             slice.selectAll("rect")
             .transition()
             .delay(function(d){return Math.random() * 1000})
             .duration(1000)
-            .attr("y",function(d) {return y(d.value);})
-            .attr("height",function(d){ return height - y(d.value)});
+            .attr("x",function(d) {return x1(d.value);})
+            .attr("width",function(d){ return height - y(d.value)});
         });
     }
 }
